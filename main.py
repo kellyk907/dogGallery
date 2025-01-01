34363fbb6e2cd650a1b9affd88c3d33ab7611104
kelly_k <pinkflamingo681@gmail.com>
2024-12-31 21:39:30 -0500

    Added drop down menu to select dog breed that gives 30 results per breed. Used GET POST method with HTML CSS Flask

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+import requests
+
+#imports a dictionary of dog data and "prettifies" the dog names when they appear in the HTML page
+from dog_breeds import prettify_dog_breed
 
 app = Flask("app")
 
-@app.route("/")
-def hello_world():
+#function adds a dash in between breed names with multiple words like miniature poodle
+def check_breed(breed):
+  return "/".join(breed.split("-"))
+
+@app.route("/", methods=["GET","POST"])
+def dog_image_gallery():
+  errors = []
+  if request.method == "POST":
+    breed = request.form.get("breed")
   return render_template("dogs.html")
 
+
+app.debug = True
 app.run(host='0.0.0.0', port=8080)
